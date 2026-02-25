583f247f19eae9d33915afd0d768604887e44fea
Athul Sudheer <athulsudheer2007@gmail.com>
2026-02-25 19:10:45 +0530

    Restore backend as normal folder

--- azurebay_backend/reservations/urls.py
+++ azurebay_backend/reservations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
+    path('favicon.ico', lambda r: HttpResponse(status=204)),
     path('', views.index_view, name='index'),
     path('booking/', views.booking_view, name='booking'),
     path('reserve/', views.create_reservation, name='create_reservation'),
